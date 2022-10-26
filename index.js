cdc8ec526659a5c0170a022b99c62a9c1a46edca
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2022-10-26 21:24:19 +0600

    corse addeded

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 var cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const course = require("./Data/course.json");
+
 const categories = require("./Data/categories.json");
 app.use(cors());
 
@@ -11,6 +13,24 @@ app.get("/course-categories", (req, res) => {
 });
 app.get("/", (req, res) => {
   res.send("Course Api Running");
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = course.find((c) => c._id === id);
+  console.log(id);
+  res.send(selectedCourse);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (id === "08") {
+    res.send(course);
+  } else {
+    const category_course = course.filter((c) => c.category_id == id);
+    res.send(category_course);
+  }
 });
 
 app.listen(port, () => {
